0597c542dc55c10298c9ca0b98ef08e39eefd9cb
code_overflowed <neerajsingla380@gmail.com>
2026-07-11 23:13:07 +0530

    String and String Functions

---
+++ Python/01_string.py
@@ -0,0 +1,24 @@
+str= "hello my name is Dhanraj"
+print(str,type(str))
+
+multi_line_str='''Hello what are you doing?
+Is everything good?'''
+print(multi_line_str)
+
+first_name="John"
+last_name="Doe"
+
+# String Concatenation
+print(first_name+last_name)
+# String Length
+print(len(str))
+# String Upper Case
+print(str.upper())
+# String Lower Case
+print(str.lower())
+# String Title Case
+print(str.title())
+# Formatted Strings
+print(f"My full name is {first_name} {last_name}")
+# Slicing
+print(str[0:10:2])
